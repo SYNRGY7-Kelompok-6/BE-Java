27a1a42dce1b7c970cf03bcbc3aee6b7c088ce45
n9mi <n9mi.me@gmail.com>
2024-07-25 06:56:37 +0700

    fix: custom error message

--- src/main/java/com/kelp_6/banking_apps/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kelp_6/banking_apps/exception/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.kelp_6.banking_apps.exception;
 
+import com.kelp_6.banking_apps.model.web.WebResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,86 +15,132 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.AccessDeniedException;
 import java.security.SignatureException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = { ConstraintViolationException.class })
-    public ProblemDetail constraintViolationException(ConstraintViolationException exception) {
+    public ResponseEntity<WebResponse<Object>> constraintViolationException(ConstraintViolationException exception) {
         log.info("[ {} ] {}", HttpStatus.BAD_REQUEST, exception.getMessage());
 
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
-        problemDetail.setProperty("description", exception.getCause());
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("BAD REQUEST")
+                .message(exception.getMessage())
+                .data(null)
+                .build();
 
-        return problemDetail;
+        return new ResponseEntity<>(errResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ProblemDetail methodArgumentNotValidException(MethodArgumentNotValidException exception){
+    public ResponseEntity<WebResponse<Object>> methodArgumentNotValidException(MethodArgumentNotValidException exception){
         log.info("[ {} ] {}", HttpStatus.BAD_REQUEST, exception.getMessage());
 
-        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-        Map<String, String> errors = new HashMap<>();
-        for(FieldError error : fieldErrors){
-            errors.put(error.getField(), error.getDefaultMessage());
-        }
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("BAD REQUEST")
+                .message(exception.getMessage())
+                .data(null)
+                .build();
 
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "invalid data");
-        problemDetail.setProperty("description", "requests contains invalid parameter");
-        problemDetail.setProperty("errors", errors);
-        return problemDetail;
+        return new ResponseEntity<>(errResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = { BadCredentialsException.class })
-    public ProblemDetail badCredentialsException(BadCredentialsException exception) {
+    public ResponseEntity<WebResponse<Object>> badCredentialsException(BadCredentialsException exception) {
         log.info("[ {} ] {}", HttpStatus.UNAUTHORIZED, exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, "unauthorized");
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("UNAUTHORIZED")
+                .message("bad credentials")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = {UsernameNotFoundException.class})
-    public ProblemDetail usernameNotFoundException(UsernameNotFoundException exception){
-        log.info("[ {} ] {}", HttpStatus.BAD_REQUEST, exception.getMessage());
+    public ResponseEntity<WebResponse<Object>> usernameNotFoundException(UsernameNotFoundException exception){
+        log.info("[ {} ] {}", HttpStatus.NOT_FOUND, exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "bad request");
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("NOT FOUND")
+                .message("user not found")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = { AccessDeniedException.class })
-    public ProblemDetail accessDeniedException(AccessDeniedException exception) {
-        log.info("[ {} ] {}", HttpStatus.FORBIDDEN, exception.getMessage());
+    public ResponseEntity<WebResponse<Object>> accessDeniedException(AccessDeniedException exception) {
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("FORBIDDEN")
+                .message("unauthorized user")
+                .data(null)
+                .build();
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, "forbidden");
+        return new ResponseEntity<>(errResponse, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(value = { ExpiredJwtException.class })
-    public ProblemDetail expiredJwtException(ExpiredJwtException exception) {
-        log.info("[ {} ] {}", HttpStatus.FORBIDDEN, exception.getMessage());
+    public ResponseEntity<WebResponse<Object>> expiredJwtException(ExpiredJwtException exception) {
+        log.info("[ {} ] {}", HttpStatus.UNAUTHORIZED, exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, "expired credentials");
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("UNAUTHORIZED")
+                .message("expired credentials")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = { SignatureException.class })
-    public ProblemDetail signatureException(SignatureException exception) {
-        log.info("[ {} ] {}", HttpStatus.FORBIDDEN, exception.getMessage());
+    public ResponseEntity<WebResponse<Object>> signatureException(SignatureException exception) {
+        log.info("[ {} ] {}", HttpStatus.UNAUTHORIZED, exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, "invalid credentials");
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status(HttpStatus.UNAUTHORIZED.toString())
+                .message("invalid credentials")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = { ResponseStatusException.class })
-    public ProblemDetail applicationException(ResponseStatusException exception) {
+    public ResponseEntity<WebResponse<Object>> applicationException(ResponseStatusException exception) {
         log.info("[ {} ] {}", exception.getStatusCode(), exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(exception.getStatusCode(), exception.getReason());
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status(exception.getStatusCode().toString().substring(4))
+                .message(exception.getReason())
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, exception.getStatusCode());
     }
 
     @ExceptionHandler(value = { Exception.class })
-    public ProblemDetail allException(Exception exception) {
+    public ResponseEntity<WebResponse<Object>> allException(Exception exception) {
         log.info("[ {} ] {}", HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "something wrong");
+        WebResponse<Object> errResponse = WebResponse
+                .<Object>builder()
+                .status("INTERNAL SERVER ERROR")
+                .message("something wrong")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(errResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
