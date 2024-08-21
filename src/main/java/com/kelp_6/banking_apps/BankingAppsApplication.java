b1fe3aa9f8d26e10b17ee62a13f75d826ddd21a1
n9mi <n9mi.me@gmail.com>
2024-08-21 20:54:22 +0700

    fix: openapi default url

--- src/main/java/com/kelp_6/banking_apps/BankingAppsApplication.java
+++ src/main/java/com/kelp_6/banking_apps/BankingAppsApplication.java
@@ -1,6 +1,8 @@
 package com.kelp_6.banking_apps;
 
 import com.kelp_6.banking_apps.setup.DatabaseSeeder;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
+@OpenAPIDefinition(servers = { @Server(url = "/api/v1.0/", description = "Default Server URL") })
 public class BankingAppsApplication {
 
 	public static void main(String[] args) {
